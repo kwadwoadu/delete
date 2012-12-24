1072d4776ffb7e9649d897142876fb02b67fff98
Kwadwo Adu <k@adu.dk>
2012-12-24 01:25:18 +0100

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Talents::Application.config.secret_token = 'f6e143f785fd85377057faa9d19dda326b42a99a02ab14e94032fdce6c0b3b8762ca8040b3c2fc6e9fe96abb59e46dfa706c5c4f46fead69f52dba7b87a0a48d'
